1c930bf65531610f15a8f08dbfd697fb6a723d4a
Viktor Sinkevich <viktor.sinkevich@specific-group.com>
2023-11-14 17:37:08 +0300

    fixed gateway

--- gateway/src/main/java/com/specificgroup/gateway/configuration/SecurityConfig.java
+++ gateway/src/main/java/com/specificgroup/gateway/configuration/SecurityConfig.java
@@ -3,33 +3,42 @@ package com.specificgroup.gateway.configuration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.config.web.server.ServerHttpSecurity.CsrfSpec;
-import org.springframework.security.config.web.server.ServerHttpSecurity.FormLoginSpec;
-import org.springframework.security.config.web.server.ServerHttpSecurity.HttpBasicSpec;
-import org.springframework.security.config.web.server.ServerHttpSecurity.LogoutSpec;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtGrantedAuthoritiesConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@RequiredArgsConstructor
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final CustomJwtConverter customJwtConverter;
+
     @Bean
-    public SecurityWebFilterChain configureResourceService(ServerHttpSecurity httpSecurity) {
-        return httpSecurity
-                .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/actuator/health/**").permitAll()
-                        .pathMatchers("/*/*/user/auth/**").permitAll()
-                        .anyExchange().authenticated()
-                )
-                .csrf(CsrfSpec::disable)
-                .httpBasic(HttpBasicSpec::disable)
-                .formLogin(FormLoginSpec::disable)
-                .logout(LogoutSpec::disable)
-                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+    public SecurityWebFilterChain configureResourceServer(ServerHttpSecurity httpSecurity) {
+
+        return httpSecurity.authorizeExchange(
+                        exchange -> exchange.pathMatchers(
+                                        "/actuator/health/**",
+                                        "/*/*/user/auth/**").permitAll()
+                                .anyExchange().authenticated())
+                .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .logout().disable()
+                .oauth2ResourceServer().jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+                .and()
                 .build();
+    }
+
+    @Bean
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new ReactiveJwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(
+                new ReactiveJwtGrantedAuthoritiesConverterAdapter(customJwtConverter));
+
+        return jwtAuthenticationConverter;
     }
 }
